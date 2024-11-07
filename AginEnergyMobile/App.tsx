e9bd57b1f977ce92ddec903cbac2c23a1080c055
Tymek Vonau <vonautymek@gmail.com>
2024-11-07 23:50:01 +0100

    [Mobile] Changed icons library

--- AginEnergyMobile/App.tsx
+++ AginEnergyMobile/App.tsx
@@ -4,7 +4,7 @@ import Home from '@screns/Home';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TabItem from '@lib/bottomTabs/TabItem';
 import TabText from '@lib/bottomTabs/TabText';
-import { IconGraph, IconHome, IconLayoutGrid, IconList, IconSettings2 } from 'tabler-icons-react-native';
+import { IconGraph, IconHome, IconLayoutGrid, IconList, IconSettings2 } from '@tabler/icons-react-native';
 import { BlurView } from 'expo-blur';
 import { useMemo } from 'react';
 import { useColors } from '@lib/hooks';
@@ -12,8 +12,8 @@ import { useColors } from '@lib/hooks';
 
 const Tab = createBottomTabNavigator();
 
-export default function App() { 
-    const {backgroundColor} = useColors();
+export default function App() {
+    const { backgroundColor } = useColors();
 
     const styles = useMemo(() => StyleSheet.create({
         container: {
@@ -49,19 +49,19 @@ export default function App() {
                 },
                 tabBarBackground: () => Platform.OS == 'ios' ? <BlurView intensity={90} tint="dark" style={styles.tabsBackground} /> : <View style={[styles.tabsBackground, styles.androidBackground]}></View>,
             })}>
-                <Tab.Screen name="Home"  component={Home} options={{
+                <Tab.Screen name="Home" component={Home} options={{
                     tabBarIcon: ({ focused, }) => <TabItem icon={IconHome} active={focused} />,
                     tabBarLabel: ({ focused }) => <TabText active={focused}>Home</TabText>,
                 }} />
-                <Tab.Screen name="Devices"  component={Home} options={{
+                <Tab.Screen name="Devices" component={Home} options={{
                     tabBarIcon: ({ focused, }) => <TabItem icon={IconLayoutGrid} active={focused} />,
                     tabBarLabel: ({ focused }) => <TabText active={focused}>Urządzenia</TabText>,
                 }} />
-                <Tab.Screen name="Stats"  component={Home} options={{
+                <Tab.Screen name="Stats" component={Home} options={{
                     tabBarIcon: ({ focused, }) => <TabItem icon={IconGraph} active={focused} />,
                     tabBarLabel: ({ focused }) => <TabText active={focused}>Statystyki</TabText>,
                 }} />
-                <Tab.Screen name="Settings"  component={Home} options={{
+                <Tab.Screen name="Settings" component={Home} options={{
                     tabBarIcon: ({ focused, }) => <TabItem icon={IconSettings2} active={focused} />,
                     tabBarLabel: ({ focused }) => <TabText active={focused}>Ustawienia</TabText>,
                 }} />
